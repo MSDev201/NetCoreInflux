1e93891cd2736e52b061c7089130751f7ee05e93
Marek Schmidt <info@msdev.me>
2019-11-30 16:22:24 +0100

    Added service extension for adding client service di

--- InfluxNetCore/Extensions/ServiceCollectionExtension.cs
+++ InfluxNetCore/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,8 +11,57 @@ namespace InfluxNetCore.Extensions
 
         public static void AddInfluxClient(this IServiceCollection service, string host = "localhost", ushort port = 8086, string username = null, string password = null)
         {
+            // register connection
+            var connection = new InfluxConnection
+            {
+                Host = host,
+                Port = port,
+                Username = username,
+                Password = password
+            };
+
+            InfluxClient.Connection = connection;
+
             // register as a service
             service.AddSingleton<IInfluxClient, InfluxClient>();
+        }
+
+        public static void AddInfluxClient(this IServiceCollection services)
+        {
+            var serviceProv = services.BuildServiceProvider();
+            var config = serviceProv.GetService<IConfiguration>();
+            var section = config.GetSection("InfluxClient");
+
+            if (!section.Exists())
+                return;
+
+            var sectionChilds = section.GetChildren();
+            string host = null;
+            ushort port = 0;
+            string username = null, password = null;
+            foreach(var sectionChild in sectionChilds)
+            {
+                switch (sectionChild.Key.ToLower())
+                {
+                    case "host":
+                        host = sectionChild.Value;
+                        break;
+                    case "port":
+                        ushort.TryParse(sectionChild.Value, out port);
+                        break;
+                    case "username":
+                        username = sectionChild.Value;
+                        break;
+                    case "password":
+                        password = sectionChild.Value;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            AddInfluxClient(services, host, port, username, password);
         }
 
     }
