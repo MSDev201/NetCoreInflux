50c1ef21bb6d03cad1ad9120a23a3b3cbac2ebd9
Marek Schmidt <info@msdev.me>
2019-11-30 16:23:31 +0100

    Added ping client for checking if influx is up and running (needs some rework)

---
+++ InfluxNetCore/Clients/PingClient.cs
@@ -0,0 +1,18 @@
+﻿using InfluxNetCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfluxNetCore.Clients
+{
+    internal class PingClient
+    {
+        public static async Task MakePingRequest()
+        {
+            await InfluxRequest.MakeGetAsync("ping");
+        }
+
+    }
+}
