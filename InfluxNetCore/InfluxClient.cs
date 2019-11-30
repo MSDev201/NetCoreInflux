dc535c5674970374d223e4926c23dae3efa500d4
Marek Schmidt <info@msdev.me>
2019-11-30 16:21:58 +0100

    added connection and client service

--- InfluxNetCore/InfluxClient.cs
+++ InfluxNetCore/InfluxClient.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using InfluxNetCore.Clients;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace InfluxNetCore
 {
     public class InfluxClient : IInfluxClient
     {
+        internal static InfluxConnection Connection { get; set; }
+
+
+        public async Task Test()
+        {
+            new Task(() =>
+            {
+                while(true)
+                {
+                    _ = WriteClient.MakeWriteRequest("testMeasure", new
+                    {
+                        Zufallszahl = new Random().Next(0, 10000),
+                        Zufalldouble = new Random().NextDouble() * 10000d,
+                    }, new
+                    {
+                        Valide = true,
+                        Invalide = false,
+                        Yesterday = DateTime.Now.AddDays(-1)
+                    });
+                    Thread.Sleep(50);
+                }
+            }).Start();
+
+            
+        }
     }
 }
---
+++ InfluxNetCore/InfluxConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfluxNetCore
+{
+    internal class InfluxConnection
+    {
+        public string Host { get; set; }
+        public ushort Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
