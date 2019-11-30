c0afa248cd87b4ae1335cca53fd8d10ce46b9772
Marek Schmidt <info@msdev.me>
2019-11-30 16:25:17 +0100

    Testing on WepApi

---
+++ TestInfluxClient/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InfluxNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestInfluxClient.Controllers
+{
+    [Route("test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IInfluxClient _client;
+
+        public TestController(IInfluxClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestStuff()
+        {
+            await _client.Test();
+            return Ok("OK");
+        }
+
+    }
+}
