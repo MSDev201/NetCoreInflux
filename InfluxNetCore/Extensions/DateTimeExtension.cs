30a697c9a2c58211269a83bf1359e60c928cc5e1
Marek Schmidt <info@msdev.me>
2019-11-30 16:23:59 +0100

    Added datetime extension for nanosecond unix timestamp

---
+++ InfluxNetCore/Extensions/DateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfluxNetCore.Extensions
+{
+    internal static class DateTimeExtension
+    {
+
+        public static ulong GetUnixTimestampNanoseconds(this DateTime datetime)
+        {
+            return (ulong)((datetime - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds * 1e6);
+        }
+
+    }
+}
